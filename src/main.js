df2de520949c346bbc0700cd3c20b1c43dfb234b
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-10-29 10:52:48 -0300

    ok

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import e from "express";
-import path from "path";
 import "dotenv/config";
 import "./config/db.js";
 import species_router from "./routers/species_router.js";
@@ -8,20 +7,8 @@ import expedition_router from "./routers/expedition_router.js";
 
 const app = e();
 app.use(e.json());
-
-// Configurar a pasta 'public' como repositório de arquivos estáticos
-app.use(e.static(path.join(process.cwd(), "src/public")));
-
-// Rotas da API
 app.use("/species", species_router);
 app.use("/explorer", explorer_router);
 app.use("/expedition", expedition_router);
 
-// Rota principal para servir o front-end
-app.get("/", (req, res) => {
-  res.sendFile(path.join(process.cwd(), "src/public/index.html"));
-});
-
-app.listen(process.env.API_PORT, () =>
-  console.log("Server Running on port", process.env.API_PORT)
-);
+app.listen(process.env.API_PORT, () => console.log("Server Running"));
